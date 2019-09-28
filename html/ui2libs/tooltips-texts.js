873aed22e0a13ba47fd7f9a4b62d21aed91aeb82
Kenneth Reitz <me@kennethreitz.org>
2019-09-28 11:37:35 -0400

    kr genome

---
+++ html/ui2libs/tooltips-texts.js
@@ -0,0 +1,33 @@
+var tooltipsTexts={};
+
+tooltipsTexts["pscore"]="Does the call rate for this platform match the expected rate based on sequencing?";
+tooltipsTexts["Magnitude"]="Magnitude is a subjective measure of interest. Over time it should be adjusted up or down by the community";
+tooltipsTexts["Repute"]="Either Good, Bad, or blank";
+tooltipsTexts["CLNSIG"]="The significance of the position (but not necessarily your genotype) according to ClinVar";
+tooltipsTexts["Frequency"]="How common is this genotype in your chosen nearest populations (ie Caucasian or Japanese)";
+tooltipsTexts["PopFreqGraph"]="Black indicates how common your genotype is among other populations. More info at https://www.SNPedia.com/index.php/PopFreqGraph";
+tooltipsTexts["GMAF"]="The frequency of the minor allele in the 1000 Genomes Project";
+tooltipsTexts["Count"]="How many times this genotype was seen int your data";
+tooltipsTexts["Gene"]="HUGO Name of the gene";
+tooltipsTexts["Chromosome"]="Chromosome";
+tooltipsTexts["Position"]="Position on the Chromosome";
+tooltipsTexts["Max Magnitude"]="Highest Magnitude across all genotypes of this SNP";
+tooltipsTexts["Geno time"]="Timestamp YMD when the genotype page was last modified";
+tooltipsTexts["Rs time"]="Timestamp YMD when the snp page was last modified";
+tooltipsTexts["Mom"]="Mom";
+tooltipsTexts["Dad"]="Dad";
+tooltipsTexts["Parent1"]="Which allele was provided by Parent1, for phased data";
+tooltipsTexts["Parent2"]="Which allele was provided by Parent2, for phased data";
+tooltipsTexts["Stabilized"]="See the snpedia page for StabilizedOrientation";
+tooltipsTexts["Orientation"]="See the snpedia page for Orientation";
+tooltipsTexts["References"]="The approximate number of published papers";
+tooltipsTexts["LinkedTo"]="Associations in SNPedia";
+tooltipsTexts["Ambiguous"]="Warning! This position is prone to confusion in the scientific literature";
+tooltipsTexts["Probability"]="Likelihood of this genotype occuring in an offspring of 2 parents";
+
+
+/*for(var prop in tooltipsTexts)
+{
+    tooltipsTexts[prop]=prop;
+}*/
+
